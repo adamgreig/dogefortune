432a30707bddebbd7b7ca5d0cfd29fdbff4c0ce7
Adam Greig <adam@adamgreig.com>
2013-12-18 17:31:37 +0000

    only use any given qualifier once

--- dogefortune/fortune.py
+++ dogefortune/fortune.py
@@ -7,11 +7,11 @@ fortunepath = os.path.join(os.path.realpath(
 fortunes = open(fortunepath).read().lower().replace("'", "").split("\n")
 
 qualifiers = {
-    "NNS": ["very", "so", "much", "such"],
-    "NN": ["very", "so", "many"],
-    "VBG": ["very", "so", "many"],
-    "VBN": ["very", "so", "many"],
-    "VB": ["so", "plz"]
+    "NNS": set(("very", "so", "much", "such")),
+    "NN": set(("very", "so", "many")),
+    "VBG": set(("very", "so", "many")),
+    "VBN": set(("very", "so", "many")),
+    "VB": set(("so", "plz"))
 }
 
 ends = [". wow", ". amaze", ". omg", ""]
@@ -23,12 +23,17 @@ def get_fortune():
     fortune = random.choice(fortunes)
     tags = TB(fortune).tags
     sentence = []
+    used_qualifiers = set()
     for tag in tags:
         if tag[0] in no:
             continue
         if tag[1] in qualifiers:
-            adj = random.choice(qualifiers[tag[1]])
-            sentence.append(adj + " " + tag[0].lower())
+            choices = qualifiers[tag[1]] - used_qualifiers
+            if not choices:
+                continue
+            qual = random.choice(list(choices))
+            used_qualifiers.add(qual)
+            sentence.append(qual + " " + tag[0].lower())
     if not sentence:
         return get_fortune()
     return ', '.join(sentence) + random.choice(ends)
